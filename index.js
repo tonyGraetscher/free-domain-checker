e94fb803aba793bc1c86f44d6ad1addd4e723d82
tonyGraetscher <50678127+tonyGraetscher@users.noreply.github.com>
2023-05-13 00:49:33 +0200

    Output file, best doc
    new config.json.example
    new config Output file
    new Domain list more TLDs possible
    new Domain list Comment with # or //

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const whois = require("whois");
-const nodemailer = require("nodemailer");
-const fs = require("fs");
+import chalk from "chalk";
+import fs from "fs";
+import path from "path";
+import nodemailer from "nodemailer";
+import whois from "whois";
 
-const config = require("./config.json");
-const domains = fs.readFileSync("./domains.txt").toString().split("\r\n");
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+//const whois = require("whois");
+//const nodemailer = require("nodemailer");
+//const fs = require("fs");
 
 function checkDomain(domain) {
 	return new Promise((resolve, reject) => {
@@ -44,42 +50,132 @@ function checkDomain(domain) {
 }
 
 async function main() {
+	console.log(
+		chalk.blue(`
+ /$$$$$$$                                    /$$                  /$$$$$$  /$$                           /$$                          
+| $$__  $$                                  |__/                 /$$__  $$| $$                          | $$                          
+| $$  \\ $$  /$$$$$$  /$$$$$$/$$$$   /$$$$$$  /$$ /$$$$$$$       | $$  \\__/| $$$$$$$   /$$$$$$   /$$$$$$$| $$   /$$  /$$$$$$   /$$$$$$ 
+| $$  | $$ /$$__  $$| $$_  $$_  $$ |____  $$| $$| $$__  $$      | $$      | $$__  $$ /$$__  $$ /$$_____/| $$  /$$/ /$$__  $$ /$$__  $$
+| $$  | $$| $$  \\ $$| $$ \\ $$ \\ $$  /$$$$$$$| $$| $$  \\ $$      | $$      | $$  \\ $$| $$$$$$$$| $$      | $$$$$$/ | $$$$$$$$| $$  \\__/
+| $$  | $$| $$  | $$| $$ | $$ | $$ /$$__  $$| $$| $$  | $$      | $$    $$| $$  | $$| $$_____/| $$      | $$_  $$ | $$_____/| $$      
+| $$$$$$$/|  $$$$$$/| $$ | $$ | $$|  $$$$$$$| $$| $$  | $$      |  $$$$$$/| $$  | $$|  $$$$$$$|  $$$$$$$| $$ \\  $$|  $$$$$$$| $$      
+|_______/  \\______/ |__/ |__/ |__/ \\_______/|__/|__/  |__/       \\______/ |__/  |__/ \\_______/ \\_______/|__/  \\__/ \\_______/|__/                                                                                                                                                                                       
+`)
+	);
+	console.log(chalk.green("Hallo, checken my GitHub-Profil \u{1F680} ") + chalk.blue("https://github.com/tonyGraetscher"));
+
+	if (!fs.existsSync("./config.json")) {
+		console.log(chalk.red("Please create a config.json file. You can use the config.json.example file as a template."));
+		process.exit(1);
+	}
+	const config = require("./config.json");
+
+	const domainFile = config.domainFile || "domains.txt";
+	if (!fs.existsSync(domainFile)) {
+		console.log(chalk.red(`Please create a ${domainFile} file.`));
+		process.exit(1);
+	}
+	const domains = fs.readFileSync(domainFile, "utf8").toString().split("\n");
+	const domainList = [];
 	const checkDomains = [];
 
-	// create reusable transporter object using the default SMTP transport
-	const transporter = nodemailer.createTransport({
-		host: config.host,
-		port: config.port,
-		secure: false, // true for 465, false for other ports
-		auth: {
-			user: config.user, // generated ethereal user
-			pass: config.pass, // generated ethereal password
-		},
-	});
-
+	// domains checken
 	for (let i = 0; i < domains.length; i++) {
-		const domain = domains[i];
-		console.log(`domain: [${i + 1}/${domains.length}]`, domain);
-		checkDomains.push(await checkDomain(domain));
-	}
-	console.log("finish check domains");
+		let domain = domains[i];
+		// remove \n and \r
+		domain = domain.split("\n")[0].split("\r")[0];
+		// remove # and // comments
+		domain = domain.split(/#(.*)/)[0].split(/\/\/(.*)/)[0];
 
-	//console.log('checkDomains:', checkDomains)
-
-	const freeDomains = checkDomains.filter((domain) => domain.isFree);
-	console.log("freeDomains:", freeDomains);
-
-	if (freeDomains.length > 0) {
-		const freeDomainsText = `This free Domains: \r\n${freeDomains.map((domain) => domain.domain).join("\r\n")}`;
-		if (config.send) {
-			await transporter.sendMail({
-				from: config.from, // sender address
-				to: config.to, // list of receivers
-				subject: "Domains are free", // Subject line
-				text: freeDomainsText, // plain text body
-			});
+		// check if domain is not in list and not empty and not space
+		if (!domainList.includes(domain) && domain !== "") {
+			const domainParts = domain.split(".");
+			const domainTLD = domainParts[domainParts.length - 1];
+			const domainTLDList = domainTLD.split("/");
+			// check if domain has more than one TLD
+			if (domainTLDList.length > 1) {
+				// add all TLDs to domainList
+				for (let i = 0; i < domainTLDList.length; i++) {
+					const domainTLD = domainTLDList[i];
+					domainParts[domainParts.length - 1] = domainTLD;
+					const newDomain = domainParts.join(".");
+					domainList.push(newDomain);
+				}
+			} else {
+				// add domain to domainList
+				domainList.push(domain);
+			}
 		}
 	}
+
+	console.log("start check domains");
+	// check domains
+	for (let i = 0; i < domainList.length; i++) {
+		const domain = domainList[i];
+		process.stdout.clearLine(0);
+		process.stdout.cursorTo(0);
+		process.stdout.write(`domain: [${i + 1}/${domainList.length}] ${domain}`);
+		checkDomains.push(await checkDomain(domain));
+	}
+	process.stdout.clearLine(0);
+	process.stdout.cursorTo(0);
+	process.stdout.write(`domain: [${domainList.length}/${domainList.length}]`);
+	process.stdout.write("\n");
+	console.log(chalk.green("finish check domains"));
+
+	const freeDomains = checkDomains.filter((domain) => domain.isFree);
+
+	console.log(chalk.gray(`Totel Domains: ${domainList.length}`));
+	console.log(chalk.gray(`Free Domains: ${freeDomains.length}`));
+	console.log(chalk.gray(`Not Free Domains: ${domainList.length - freeDomains.length}`));
+
+	if (config.consoleOutput === "free") {
+		console.log("freeDomains:", freeDomains);
+	} else if (config.consoleOutput === "all") {
+		console.log("checkDomains:", checkDomains);
+	}
+
+	// send mail
+	if (config.send) {
+		// check config
+		if (!config.host || !config.port || !config.user || !config.pass || !config.from || !config.to) {
+			console.log(chalk.red("Please fill in all fields in the config.json file."));
+			process.exit(1);
+		}
+		// create reusable transporter object using the default SMTP transport
+		const transporter = nodemailer.createTransport({
+			host: config.host,
+			port: config.port,
+			secure: false, // true for 465, false for other ports
+			auth: {
+				user: config.user, // generated ethereal user
+				pass: config.pass, // generated ethereal password
+			},
+		});
+
+		// send mail with defined transport object
+		let mailText = `This free Domains: \r\n${freeDomains.map((domain) => domain.domain).join("\r\n")}`;
+		if (config.mailOutput === "all") {
+			mailText = `This all Domains: \r\n${checkDomains.map((domain) => `${domain.isFree ? "Free" : "forgive"} ${domain.domain}`).join("\r\n")}`;
+		}
+		await transporter.sendMail({
+			from: config.from, // sender address
+			to: config.to, // list of receivers
+			subject: "Free Domain Checker", // Subject line
+			text: mailText, // plain text body
+		});
+		console.log(chalk.green("send mail"));
+	}
+
+	// write file
+	const filePath = path.join(process.cwd(), `check-domains-${new Date().toISOString().replaceAll(":", "")}.json`);
+	if (config.outputFile === "all") {
+		fs.writeFileSync(filePath, JSON.stringify(checkDomains, null, 2));
+	} else if (config.outputFile === "free") {
+		fs.writeFileSync(filePath, JSON.stringify(freeDomains));
+	}
+
+	console.log(chalk.yellow("good bye"));
 }
 
 main().catch(console.error);
